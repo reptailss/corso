13e97ce5e3c364c3d2ce60d581d7ac53be743311
reptailss <reptailss@gmail.com>
2021-02-23 01:07:05 +0200

    efef

--- build/js/main.js
+++ build/js/main.js
@@ -14,7 +14,48 @@ sliders.forEach((el) => {
         },
         slideClass: 'slide__card',
         wrapperClass: 'slide__wrap',
-        containerClass: 'slide__container'
+        containerClass: 'slide__container',
+        autoplay: {
+            delay: 5000,
+        }
 
     });
+});
+
+var el = document.getElementsByClassName('menu-item');
+
+for (var i = 0; i < el.length; i++) {
+    el[i].addEventListener("mouseenter", showSub, false);
+    el[i].addEventListener("mouseleave", hideSub, false);
+}
+
+function showSub(e) {
+    if (this.children.length > 1) {
+        this.children[1].style.transform = "translateY(0)";
+        this.children[1].style.overflow = "visible";
+        this.children[1].style.opacity = "1";
+        this.children[1].style.height = "auto";
+
+    } else {
+        return false;
+    }
+}
+
+function hideSub(e) {
+    if (this.children.length > 1) {
+        this.children[1].style.transform = "translateY(100%)";
+        this.children[1].style.overflow = "hidden";
+        this.children[1].style.opacity = "0";
+        this.children[1].style.height = "0";
+
+    } else {
+        return false;
+    }
+}
+
+$(function() {
+    $('.burger').on('click', function() {
+        $(this).find('.burger-span').toggleClass('active');
+        $('.header-menu__list').toggleClass('active');
+    })
 });
