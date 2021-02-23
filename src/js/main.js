bec919d36b137c41be4ac1fcc01982075c997c28
reptailss <reptailss@gmail.com>
2021-02-23 18:47:58 +0200

    efefe

--- src/js/main.js
+++ src/js/main.js
@@ -31,11 +31,20 @@ for (var i = 0; i < el.length; i++) {
 
 function showSub(e) {
     if (this.children.length > 1) {
-        this.children[1].style.transform = "translateY(0)";
-        this.children[1].style.overflow = "visible";
-        this.children[1].style.opacity = "1";
-        // this.children[1].style.height = "auto";
+        if (window.matchMedia("(max-width: 800px)").matches) {
+            this.children[1].style.transform = "translateY(0)";
+            this.children[1].style.overflow = "visible";
+            this.children[1].style.opacity = "1";
+            this.children[1].style.height = "auto";
+            this.children[1].style.display = "block";
 
+        } else {
+            this.children[1].style.transform = "translateY(0)";
+            this.children[1].style.overflow = "visible";
+            this.children[1].style.opacity = "1";
+            this.children[1].style.height = "auto";
+            this.children[1].style.display = "block";
+        }
     } else {
         return false;
     }
@@ -43,10 +52,21 @@ function showSub(e) {
 
 function hideSub(e) {
     if (this.children.length > 1) {
-        this.children[1].style.transform = "translateY(100%)";
-        this.children[1].style.overflow = "hidden";
-        this.children[1].style.opacity = "0";
-        // this.children[1].style.height = "0";
+        if (window.matchMedia("(max-width: 800px)").matches) {
+            this.children[1].style.height = "0";
+            this.children[1].style.transform = "translateY(100%)";
+            this.children[1].style.overflow = "hidden";
+            this.children[1].style.opacity = "0";
+            this.children[1].style.display = "none";
+        } else {
+
+            this.children[1].style.transform = "translateY(100%)";
+            this.children[1].style.overflow = "hidden";
+            this.children[1].style.opacity = "0";
+            this.children[1].style.display = "none";
+        }
+
+
 
     } else {
         return false;
@@ -57,5 +77,9 @@ $(function() {
     $('.burger').on('click', function() {
         $(this).find('.burger-span').toggleClass('active');
         $('.header-menu__list').toggleClass('active');
+    })
+
+    $('.footer__item-js').on('click', function() {
+        $(this).find('.footer__item-list-js').toggleClass('active');
     })
 });
